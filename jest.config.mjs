501a2b5d097f42cd2d2dad426d0d6767ea7f34a4
chibakoo0130 <kohtarocb@gmail.com>
2025-02-21 09:15:08 +0900

    change jest setup setting

--- jest.config.mjs
+++ jest.config.mjs
@@ -4,7 +4,7 @@ const createJestConfig = nextJest({ dir: './' });
 
 const customJestConfig = {
     testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
     testEnvironment: 'jsdom',
 }
 
